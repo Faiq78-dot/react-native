789694703a294fcc066759ef8b9fa05d94a22c24
Faiq78-dot <faiqmaqsood84@gmail.com>
2024-11-24 17:31:30 +0500

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,104 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, FlatList, TextInput, ScrollView, TouchableOpacity, StyleSheet, Text,  View } from 'react-native';
+import {Feather} from '@expo/vector-icons';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+               
+  const Chatdata=[
+    {id : '1' , Name : 'Faiq' , message: 'kya haal ha' },
+    {id : '2' , Name : 'Aleem' , message: 'ok'},
+    {id : '3' , Name : 'Bilal' , message : 'Chal Yaar kal miltay hn'},
+    {id : '4' , Name : 'Asad' , message : 'done'},
+    {id : '5' , Name : 'Anns' , message : 'Chal miltay hn'},
+    {id : '5' , Name : 'Haaris' , message : 'Cable dedi'},
+   
+]
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+const App =()=>{
+    return(
+        <View>
+        <View style={style.Textinput}>
+        <Text style={style.Text}>WhatsApp</Text>
+        <View style={style.Headericons}>
+        <Feather 
+        name='camera' size={24} />
+        <Feather 
+        name='search' size={24} style={style.Iconmargin} />   
+        <Feather
+        name='more-vertical' size={24} />
+        </View>
+        </View>
+        <View style={style.Searchicon}>
+            <Feather
+            name='search' size={24}/>
+        <TextInput
+        style={style.Searchinput}
+        placeholder='Ask Meta AI or Search' ></TextInput>
+        </View>
+        <View> 
+            <ScrollView>
+        <FlatList
+        data={Chatdata}
+        renderItem={({item})=><View 
+        style={style.Chats} ><Text style={style.Name} >{item.Name}</Text>
+        <Text>{item.message}</Text></View>}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
-}
+        </ScrollView>
+        </View>
+        <View style={style.Bottombuttons}>
+            <Text style={{fontWeight:'bold'}}>Chats</Text>
+            <Text style={{fontWeight:'bold'}}>Updates</Text>
+            <Text style={{fontWeight:'bold'}}>Communities</Text>
+            <Text style={{fontWeight:'bold'}}>Status</Text>
+        </View>
+        </View>
+    )
+};
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+const style=StyleSheet.create({
+    
+    Textinput :{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        padding:8,
+    },
+    Text:{
+        fontSize: 25,
+        color: 'green',
+        fontWeight:'bold'
+    },
+    Headericons:{
+        flexDirection:'row'
+    },
+    Iconmargin:{
+        marginHorizontal:15
+    },
+    Searchicon:{
+        flexDirection:'row',
+        alignItems:'center',
+        margin:10,
+        paddingHorizontal:10,
+        borderWidth:1,
+        borderRadius: 10
+    },
+    Searchinput:{
+        flex:1,
+        paddingHorizontal:10,
+        
+    },
+    Chats:{
+        padding:4,        
+        margin:2,
+    },
+    Name:{
+        fontWeight:'bold'
+    },
+    Bottombuttons:{
+        flexDirection:'row',
+        justifyContent:'space-around',
+        padding: 5,
+        borderTopWidth:1
+    },
+})
+
+
+export default App;
