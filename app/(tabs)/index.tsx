cc1b697cb9034dc0f47cc214be176a81ab0974d8
Faiq78-dot <faiqmaqsood84@gmail.com>
2024-12-03 15:22:23 +0500

    New change

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,104 +1,24 @@
 import { Image, FlatList, TextInput, ScrollView, TouchableOpacity, StyleSheet, Text,  View } from 'react-native';
 import {Feather} from '@expo/vector-icons';
+import { NavigationContainer, NavigationIndependentTree } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../Home';
+import { HeaderTitle } from '@react-navigation/elements';
 
-               
-  const Chatdata=[
-    {id : '1' , Name : 'Faiq' , message: 'kya haal ha' },
-    {id : '2' , Name : 'Aleem' , message: 'ok'},
-    {id : '3' , Name : 'Bilal' , message : 'Chal Yaar kal miltay hn'},
-    {id : '4' , Name : 'Asad' , message : 'done'},
-    {id : '5' , Name : 'Anns' , message : 'Chal miltay hn'},
-    {id : '5' , Name : 'Haaris' , message : 'Cable dedi'},
-   
-]
+const Stack = createNativeStackNavigator();
 
 const App =()=>{
     return(
-        <View>
-        <View style={style.Textinput}>
-        <Text style={style.Text}>WhatsApp</Text>
-        <View style={style.Headericons}>
-        <Feather 
-        name='camera' size={24} />
-        <Feather 
-        name='search' size={24} style={style.Iconmargin} />   
-        <Feather
-        name='more-vertical' size={24} />
-        </View>
-        </View>
-        <View style={style.Searchicon}>
-            <Feather
-            name='search' size={24}/>
-        <TextInput
-        style={style.Searchinput}
-        placeholder='Ask Meta AI or Search' ></TextInput>
-        </View>
-        <View> 
-            <ScrollView>
-        <FlatList
-        data={Chatdata}
-        renderItem={({item})=><View 
-        style={style.Chats} ><Text style={style.Name} >{item.Name}</Text>
-        <Text>{item.message}</Text></View>}
-        />
-        </ScrollView>
-        </View>
-        <View style={style.Bottombuttons}>
-            <Text style={{fontWeight:'bold'}}>Chats</Text>
-            <Text style={{fontWeight:'bold'}}>Updates</Text>
-            <Text style={{fontWeight:'bold'}}>Communities</Text>
-            <Text style={{fontWeight:'bold'}}>Status</Text>
-        </View>
-        </View>
-    )
+        <NavigationIndependentTree>
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{headerShown : false}}>
+                <Stack.Screen name='Home' component={Home} />
+            </Stack.Navigator>
+        </NavigationContainer>
+        </NavigationIndependentTree>
+    );
 };
 
-const style=StyleSheet.create({
-    
-    Textinput :{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        alignItems:'center',
-        padding:8,
-    },
-    Text:{
-        fontSize: 25,
-        color: 'green',
-        fontWeight:'bold'
-    },
-    Headericons:{
-        flexDirection:'row'
-    },
-    Iconmargin:{
-        marginHorizontal:15
-    },
-    Searchicon:{
-        flexDirection:'row',
-        alignItems:'center',
-        margin:10,
-        paddingHorizontal:10,
-        borderWidth:1,
-        borderRadius: 10
-    },
-    Searchinput:{
-        flex:1,
-        paddingHorizontal:10,
-        
-    },
-    Chats:{
-        padding:4,        
-        margin:2,
-    },
-    Name:{
-        fontWeight:'bold'
-    },
-    Bottombuttons:{
-        flexDirection:'row',
-        justifyContent:'space-around',
-        padding: 5,
-        borderTopWidth:1
-    },
-})
 
 
 export default App;
---
+++ app/Home.tsx
@@ -0,0 +1,105 @@
+import { Text, View, StyleSheet, TouchableOpacity, Modal, Button } from "react-native";
+import { Feather } from '@expo/vector-icons';
+import { useState } from "react";
+
+const Home = () => {
+
+    const [showModal, setshowModal] = useState(false)
+    return (
+        <View>
+            <View style={Style.Container}>
+                <Modal 
+                visible={showModal}
+                animationType="slide"
+                >
+                    <View style={{ margin: 20 }}>
+                        <Text style={{ fontSize: 30, fontWeight: 'bold', textAlign: 'center' }}>Acccess Nova </Text>
+                        <Text style={{ fontSize: 30, fontWeight: 'bold', textAlign: 'center' }}>on Desktop </Text>
+                    </View>
+                    <View style={{ flexDirection: 'row', justifyContent: 'center', margin: 30 }}>
+                        <Feather
+                            name="smartphone" size={30} color='blue' />
+                        <Feather
+                            name="repeat" size={30} color='blue' style={{ marginHorizontal: 5 }} />
+                        <Feather
+                            name="monitor" size={30} color='blue' />
+                    </View>
+                    <View>
+                        <Text style={{ fontSize: 15, textAlign: 'center' }}> You can access your Nova account on</Text>
+                        <Text style={{ fontSize: 15, textAlign: 'center' }}> Desktop & Web. Your entire chat history </Text>
+                        <Text style={{ fontSize: 15, textAlign: 'center' }}> will be synchronized across various </Text>
+                        <Text style={{ fontSize: 15, textAlign: 'center' }}> devices </Text>
+                    </View>
+
+                    <Text style={{ textAlign: 'center', marginTop: 50, fontWeight: '700', fontSize: 15 }}> Sign up for a Nova Account</Text>
+
+                    <View style={{ flexDirection: 'row', marginTop: 14 }}>
+                        <Feather
+                            name="user" size={25} style={{ marginHorizontal: 8, marginLeft: 62 }} />
+                        <Text style={{ textAlign: 'center', fontWeight: '300' }}>Sign up to create account </Text>
+                    </View>
+                    <Text style={{ fontSize: 15, marginLeft: 92, fontWeight: '300' }}> If You Already have an </Text>
+                    <Text style={{ fontSize: 15, marginLeft: 92, fontWeight: '300' }}> account , log in. </Text>
+
+                    <Text style={{ textAlign: 'center', fontWeight: '700', marginTop: 50, fontSize: 15 }}> Visit Nova on Your Desktop</Text>
+
+                    <View style={{ flexDirection: 'row' }}>
+                        <Feather
+                            name="monitor" size={25} style={{ marginLeft: 50, marginTop: 7 }} />
+                        <Text style={{ fontSize: 15, marginHorizontal: 13, marginTop: 6, fontWeight: '300' }}>Visit novaapp.ai on your </Text>
+                    </View>
+                    <Text style={{ marginLeft: 90, fontSize: 15, fontWeight: '300' }}>computer to continue your</Text>
+                    <Text style={{ marginLeft: 90, fontSize: 15, fontWeight: '300' }}>conservation there.</Text>
+
+                    <View >
+                        <TouchableOpacity onPress={()=>setshowModal(false)}>
+                            <Text style={Style.Button}>Get Started</Text>
+                        </TouchableOpacity>
+
+                    </View>
+                </Modal>
+
+                <TouchableOpacity onPress={()=>setshowModal(true)}>
+                    <Feather
+                        name="monitor" size={20} />
+                </TouchableOpacity>
+
+                <View>
+                    <TouchableOpacity>
+                        <Feather
+                            name="user" size={20} style={{ marginHorizontal: 15 }} />
+                    </TouchableOpacity>
+
+                </View>
+            </View>
+
+        </View>
+    );
+};
+const Style = StyleSheet.create({
+    Container: {
+        flexDirection: 'row',
+        marginVertical: 60,
+        justifyContent: 'flex-end',
+        marginHorizontal: 18
+
+    },
+    CenteredModal: {
+        position: 'absolute',
+        bottom: 650,
+        backgroundColor: '#fff',
+        borderRadius: 20,
+    },
+    Button: {
+        backgroundColor: '#007BFF',
+        paddingVertical: 20,
+        paddingHorizontal: 30,
+        textAlign: 'center',
+        marginTop: 80,
+        color: 'white',
+        borderRadius: 10
+    }
+
+})
+
+export default Home;
